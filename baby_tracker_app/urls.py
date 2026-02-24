2e92621900ea937bbcb8a28fb9842131c50b89d1
samalkralik <samalkralik@gmail.com>
2026-02-24 19:18:14 +0100

    Update urls.py

--- baby_tracker_app/urls.py
+++ baby_tracker_app/urls.py
@@ -14,7 +14,10 @@ urlpatterns = [
     path("overview/<int:baby_id>/", views.baby_detail_view),
     path("overview/<int:baby_id>/update/", views.baby_update_view),
     path("overview/<int:baby_id>/feeding/", views.feeding_view),
+    # path("overview/<int:baby_id>/feeding/update", views.feeding_update_view),
     path("overview/<int:baby_id>/sleep/", views.sleep_view),
+    # path("overview/<int:baby_id>/sleep/update", views.sleep_update_view),
     path("overview/<int:baby_id>/growth/", views.growth_view),
+    # path("overview/<int:baby_id>/growth/update", views.growth_update_view),
     path("accounts/profile/", views.profile_view),
 ]
