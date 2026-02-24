e37f37119794c84a3e211a022ac19945ce52bcd5
samalkralik <samalkralik@gmail.com>
2026-02-24 20:03:42 +0100

    production settings and urls changes

--- _project/urls.py
+++ _project/urls.py
@@ -17,16 +17,18 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
-from django.conf.urls.static import static
 
-urlpatterns = (
-    [
-        path("", include("baby_tracker_app.urls")),
-        path("accounts/", include("allauth.urls")),
-        path("admin/", admin.site.urls),
-    ]
-    + debug_toolbar_urls()
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-)
+
+urlpatterns = [
+    path("", include("baby_tracker_app.urls")),
+    path("accounts/", include("allauth.urls")),
+    path("admin/", admin.site.urls),
+]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from debug_toolbar.toolbar import debug_toolbar_urls
+
+    urlpatterns.extend(debug_toolbar_urls())
+    urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
